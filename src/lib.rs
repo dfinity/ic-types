447a36661c6b48693ce66a62de50252c63a9948d
Linwei Shang <linwei.shang@dfinity.org>
2022-11-22 13:02:31 -0500

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@
 //! This crate is no longer supported.
 //!
 //! Check [candid](https://crates.io/crates/candid) for `Principal`.
+//!
 //! Check [ic-certification](https://crates.io/crates/ic-certification) for `HashTree`.
